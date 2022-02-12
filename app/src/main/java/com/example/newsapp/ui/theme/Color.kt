c5307493ef196c7b85196d50308c6ada2254dcdb
Biju Mondal <biju@gozo.cab>
2022-02-12 22:39:41 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/newsapp/ui/theme/Color.kt
@@ -0,0 +1,8 @@
+package com.example.newsapp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFFFFFFF)
+val Purple500 = Color(0xFF2F2D33)
+val Purple700 = Color(0xFF505050)
+val Teal200 = Color(0xFFE4E4E4)
